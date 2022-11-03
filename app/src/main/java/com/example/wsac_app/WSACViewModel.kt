d97219333c0790b5ecc5461f26e81d9af4967ad7
AndrewTran03 <andrewtranva@gmail.com>
2022-11-03 16:50:09 -0400

    Got ViewModel Created and Made a (Not-So-Good) Recipe-Fragment View (Sorry Guys!)

---
+++ app/src/main/java/com/example/wsac_app/WSACViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.wsac_app
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class WSACViewModel (application: Application): AndroidViewModel(application) {
+
+}
